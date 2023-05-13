811b599fac9153980c6aa17bdc405575784ef56d
DuarteProg <lucas.duartebarbosa@outlook.com>
2023-05-13 17:46:48 -0300

    Toda implementação referente a pasta repository foi finalizada

--- src/repositories/main-repository.js
+++ src/repositories/main-repository.js
@@ -1,33 +1,51 @@
 import axios from "axios";
 import cheerio from "cheerio";
 
-const url = "https://www.netshoes.com.br/";
+const url =
+  "https://www.netshoes.com.br//tenis-olympikus-ultraleve-232g-feminino-off+white+coral-2I2-4961-205";
 const response = await axios.get(url);
 const html = response.data;
 const $ = cheerio.load(html);
 
-async function showTitle(){
-    const title = $("title").text();
-    return title;
+async function showTitle() {
+  const title = $(
+    "#content > div:nth-child(3) > section > div.short-showcase-description > section > h1"
+  );
+  const result = title.text();
+  console.log(result);
+  return result;
 }
 
-async function showPrize(){
-    
+async function showPrize() {
+  const prize = $(
+    "#buy-box > div.if-available > div.price.price-box > div > span:nth-child(1) > strong"
+  );
+  const result = prize.text();
+  console.log(result);
+  return result;
 }
 
-async function showImage(){
-    
+async function showImage() {
+  const image = $(
+    "#content > div:nth-child(3) > section > section.photo > figure > img"
+  );
+  const result = image.attr("src");
+  console.log(result);
+  return result;
 }
 
-async function showDescription(){
-    
+async function showDescription() {
+  const description = $("#features > p");
+  const result = description.text();
+  console.log(result);
+  return result;
 }
 
 const mainRepository = {
-    showTitle,
-    showPrize,
-    showImage,
-    showDescription
-}
+  showTitle,
+  showPrize,
+  showImage,
+  showDescription,
+};
 
 export default mainRepository;
--- src/services/main-service.js
+++ src/services/main-service.js
@@ -1,12 +1,32 @@
 import mainRepository from "../repositories/main-repository.js";
 
 async function mainLogic() {
-  const result = await mainRepository.showTitle();
-  return result;
+  const titleResult = await mainRepository.showTitle();
+  const prizeResult = await mainRepository.showPrize();
+  const imageResult = await mainRepository.showImage();
+  const descriptionResult = await mainRepository.showDescription();
+  
+  if (prizeResult.length === 0) {
+    throw new Error('Elemento de preço não encontrado');
+  }
+  
+
+  
+
+
+
+  
+  
+  return {
+    Titulo: titleResult,
+    Preço: prizeResult,
+    Imagem: imageResult,
+    Descrição: descriptionResult,
+  };
 }
 
 const mainService = {
   mainLogic,
 };
 
-export default mainService
+export default mainService;
