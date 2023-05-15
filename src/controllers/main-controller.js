6cd75d569a500e0a0bf2033b1a6a4acec867cf84
DuarteProg <lucas.duartebarbosa@outlook.com>
2023-05-14 21:38:35 -0300

    coloquei um link melhor para o print screen d aplicação

--- src/controllers/main-controller.js
+++ src/controllers/main-controller.js
@@ -8,14 +8,12 @@ export async function mainController(req, res) {
     const image = await mainService.imageLogic();
     const description = await mainService.desriptionLogic();
 
-    res
-      .status(httpStatus.OK)
-      .send({
-        Titulo: title,
-        Preço: prize,
-        Imagem: image,
-        Descrição: description,
-      });
+    res.status(httpStatus.OK).send({
+      Titulo: title,
+      Preço: prize,
+      Imagem: image,
+      Descrição: description,
+    });
   } catch (error) {
     if (error.name === "badRequesterror") {
       return res.sendStatus(httpStatus.BAD_REQUEST);
